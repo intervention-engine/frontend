375506d7e6fecd697e5ab2dfad32cb18e53b3215
Andy Gregorowicz <andy@mitre.org>
2015-08-26 14:47:02 -0400

    [WIP] Starting work on allowing the switching of assessments

--- app/components/patient-summary.js
+++ app/components/patient-summary.js
@@ -2,7 +2,25 @@ import Ember from 'ember';
 
 let PatientSummaryComponent = Ember.Component.extend({
   patient: null,
-  currentAssessment: "Stroke"
+  currentAssessment: "Stroke",
+
+  risksWithBirthdayStart: Ember.computed('patient.sortedRisks', 'patient.birthDate', 'currentAssessment', function(){
+    let birthRisk = this.get('patient.store').createRecord("risk-assessment", {date: this.get('patient.birthDate')})
+    let riskCode = this.get('patient.store').createRecord("codeable-concept", {text: "Stroke"});
+    let rapc = this.get('patient.store').createRecord("risk-assessment-prediction-component", {probabilityDecimal: 0});
+    rapc.set('outcome', riskCode);
+    birthRisk.get('prediction').pushObject(rapc);
+    let risks = [birthRisk];
+    risks.pushObjects(this.get('patient.sortedRisks'));
+    return risks.filterBy('prediction.firstObject.outcome.text', this.get("currentAssessment"));
+  }),
+
+  actions: {
+    switchAssessment: function() {
+      this.set("currentAssessment", "Negative Outcome");
+      this.sendAction();
+    }
+  }
 });
 
 export default PatientSummaryComponent;
--- app/components/patient-viewer.js
+++ app/components/patient-viewer.js
@@ -2,7 +2,30 @@ import Ember from 'ember';
 
 let PatientViewerComponent = Ember.Component.extend({
   patient: null,
-  currentAssessment: "Stroke"
+  currentAssessment: "Stroke",
+
+  slices: Ember.computed('currentAssessment', 'patient.sortedRisks.@each.pie', function(){
+    let risk = this.get('patient.sortedRisks').filterBy('prediction.firstObject.outcome.text', this.get('currentAssessment')).get('lastObject');
+    if (risk) {
+      return risk.get('pie').get('sliceArray');
+    } else {
+      return [
+      {name: 'medications', title: 'Medications', value: 0, weight: 1},
+      {name: 'conditions', title: 'Conditions', value: 0, weight: 2},
+      {name: 'readmissions', title: 'Readmissions', value: 0, weight: 1},
+      {name: 'utilization', title: 'Utilizations', value: 5, weight: 0.5},
+      {name: 'social_barriers', title: 'Social Barriers', value: 2, weight: 1},
+      {name: 'falls', title: 'Falls', value: 1, weight: 1}
+      ];
+    }
+  }),
+
+  actions: {
+    switchAssessment: function() {
+      console.log("Bubble");
+      this.set("currentAssessment", "Negative Outcome");
+    }
+  }
 });
 
 export default PatientViewerComponent;
