bf74a4d54b7c69d426dd3c76e6abd8b8f5249a63
Noranda Brown <noranda@norandabrown.com>
2016-04-21 21:52:59 -0400

    Removed more unused features for the pilot

--- app/mixins/condition-encounter-code-filters.js
+++ app/mixins/condition-encounter-code-filters.js
@@ -7,17 +7,18 @@ const AUTOCOMPLETE_ITEM_MAX = 10;
 export default Ember.Mixin.create({
   ajax: service(),
 
-  codingSystems: [
-    { url: 'http://hl7.org/fhir/sid/icd-9', system: 'ICD-9' },
-    { url: 'http://hl7.org/fhir/sid/icd-10', system: 'ICD-10' },
-    { url: 'http://snomed.info/sct', system: 'SNOMED CT' },
-    { url: 'http://loinc.org', system: 'LOINC' },
-    { url: 'http://www.hl7.org/FHIR/valueset-dicom-dcim.html', system: 'DCM' },
-    { url: 'http://unitsofmeasure.org', system: 'UCUM' },
-    { url: 'http://www.radlex.org/', system: 'RadLex' },
-    { url: 'http://www.whocc.no/atc', system: 'WHO' },
-    { url: 'urn:std:iso:11073:10101', system: 'ISO 11073-10101' }
-  ],
+  // codingSystems: [
+  //   { url: 'http://hl7.org/fhir/sid/icd-9', system: 'ICD-9' },
+  //   { url: 'http://hl7.org/fhir/sid/icd-10', system: 'ICD-10' },
+  //   { url: 'http://snomed.info/sct', system: 'SNOMED CT' },
+  //   { url: 'http://loinc.org', system: 'LOINC' },
+  //   { url: 'http://www.hl7.org/FHIR/valueset-dicom-dcim.html', system: 'DCM' },
+  //   { url: 'http://unitsofmeasure.org', system: 'UCUM' },
+  //   { url: 'http://www.radlex.org/', system: 'RadLex' },
+  //   { url: 'http://www.whocc.no/atc', system: 'WHO' },
+  //   { url: 'urn:std:iso:11073:10101', system: 'ISO 11073-10101' },
+  //   { url: 'http://www.ama-assn.org/go/cpt', system: 'CPT' }
+  // ],
 
   selectedCodingSystem: null,
 
