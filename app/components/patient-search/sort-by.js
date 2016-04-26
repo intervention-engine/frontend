7e5cf7ab4f2d0e468b8739acfdfc3a9e765054f4
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-04-26 14:28:32 -0400

    Ember Test apparently failed?
    Fixing JSHint errors.

--- app/components/patient-search/sort-by.js
+++ app/components/patient-search/sort-by.js
@@ -11,9 +11,9 @@ export default Component.extend({
         { name: 'Name', sortKey: 'name,birthdate', sortIcon: 'alpha' },
         { name: 'Age', sortKey: 'birthdate,name', sortIcon: 'numeric', invert: true },
         { name: 'Gender', sortKey: 'gender,name', sortIcon: 'alpha' }
-        //{ name: 'Location', sortKey: 'address,name', sortIcon: 'alpha' },
-        //{ name: 'Risk Score', sortKey: 'riskScore,name', sortIcon: 'numeric', defaultSortDescending: true },
-        //{ name: 'Notifications', sortKey: 'notifications,name', sortIcon: 'numeric', defaultSortDescending: true }
+        // { name: 'Location', sortKey: 'address,name', sortIcon: 'alpha' },
+        // { name: 'Risk Score', sortKey: 'riskScore,name', sortIcon: 'numeric', defaultSortDescending: true },
+        // { name: 'Notifications', sortKey: 'notifications,name', sortIcon: 'numeric', defaultSortDescending: true }
       ];
     }
   })
