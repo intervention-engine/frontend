09ea122002bee847b7cba5435cf91ff030f0ad0e
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-08-01 09:53:50 -0400

    Working on refactoring next and prev patient to be clearer.

--- app/controllers/patients/show.js
+++ app/controllers/patients/show.js
@@ -1,5 +1,6 @@
 import Controller from 'ember-controller';
 import computed from 'ember-computed';
+import observer from 'ember-metal/observer';
 import service from 'ember-service/inject';
 import controller from 'ember-controller/inject';
 import run from 'ember-runloop';
@@ -32,18 +33,34 @@ export default Controller.extend({
       return this.get('huddlePatients').indexOf(this.get('model')) + 1 + this.get('huddleOffset');
     }
   }),
-  nextPatient: computed('currentPatientIndex', 'huddlePatients.firstObject', {
+
+  patientObserver: observer('currentPatientIndex', function() {
+    let i = this.get('currentPatientIndex');
+    let prevPatientIndex = i - 1;
+    let nextPatientPage = Math.floor(prevPatientIndex/)
+    let nextPatientIndex = i + 1;
+    let currentPage = Math.floor(i / 8);
+    console.log(currentPage)
+    console.log(`PrevPatient ${prevPatientIndex} is ${(1 + prevPatientIndex/8) == currentPage? '': 'not'} on this page`)
+
+    
+    
+    
+  }),
+
+  nextPatient: computed('currentPatientIndex', {
     get() {
+
       let nextIndex = this.get('huddlePatients').indexOf(this.get('model')) + 1;
       // This handles the edge case of navigating to a patient on the next page
-      if (nextIndex >= this.get('huddlePatients.length')) {
-        return run(() => {
-          let currentPage = this.get('huddlePatients.page');
-          this.get('huddlePatients').set('page', currentPage + 1);
-          this.get('indexController').set('page', currentPage + 1);
-          return this.get('huddlePatients').get('firstObject');
-        });
-      }
+      // if (nextIndex >= this.get('huddlePatients.length')) {
+      //   return run(() => {
+      //     let currentPage = this.get('huddlePatients.page');
+      //     this.get('huddlePatients').set('page', currentPage + 1);
+      //     this.get('indexController').set('page', currentPage + 1);
+      //     return this.get('huddlePatients').get('firstObject');
+      //   });
+      // }
       return this.get('huddlePatients').toArray()[nextIndex];
     }
   }),
@@ -60,18 +77,19 @@ export default Controller.extend({
         return prevPatient
       }
       
-      // If we're on the first page we 
       let currentPage = this.get('huddlePatients.page');
       if (currentPage > 1) {
+        debugger
         let serializer = this.store.serializerFor('patient')
         let url = new URL(this.get('huddlePatients.meta.link').findBy('relation', 'previous').url)
-        console.debug(url)
-        return this.get('model')
         // // Because the prev/next links are full URL we have to strip the host off. 
-        // return this.get('ajax').request(url.pathname + url.search).then(function(res) {
-        //   debugger
-        //   return this.set('prevPatient', res.entry.reverse()[0].resource.id);
-        // });
+        self = this
+        this.get('ajax').request(url.pathname + url.search).then(function(res) {
+          debugger
+          self.set('prevPatient', res.entry.reverse()[0].resource.id);
+        });
+        return this.get('model')
+        
         
       }
       // Return nothing, this means we are on the first patient and don't want to display an arrow
@@ -98,6 +116,10 @@ export default Controller.extend({
   },
 
   actions: {
+    changeCurrentPatientIndex(amount){
+      this.set('currentPatientIndex', this.get('currentPatientIndex') + amount);
+    },
+
     setRiskAssessment(riskAssessment) {
       this.set('currentAssessment', riskAssessment);
       this.set('selectedCategory', null);
