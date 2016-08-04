233dff1386513be015f6f66b9831e11a9915adb5
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-08-04 14:50:05 -0400

    Human Readable Code Display
    Display human readable text for code systems if it's a common one.

--- app/models/codeable-concept.js
+++ app/models/codeable-concept.js
@@ -1,6 +1,17 @@
 import CodeableConcept from 'ember-fhir-adapter/models/codeable-concept';
 import computed from 'ember-computed';
 
+// We don't want to display the URI of the system, it's ugly, so here's a lookup table
+// TODO the better way to do this would be have it hucked in some config file.
+const SYSTEM_LOOKUP = {
+  'http://snomed.info/sct': 'SNOMED',
+  'http://www.nlm.nih.gov/research/umls/rxnorm': 'RxNorm',
+  'http://loinc.org': 'LOINC',
+  'http://www.ama-assn.org/go/cpt': 'CPT',
+  'http://hl7.org/fhir/sid/icd-9': 'ICD9',
+  'http://hl7.org/fhir/sid/icd-10': 'ICD10'
+};
+
 export default CodeableConcept.extend({
   hasCode(code) {
     let matchedCodes = this.get('coding').map(function(c) {
@@ -11,7 +22,7 @@ export default CodeableConcept.extend({
 
   displayText: computed('text', 'coding.firstObject.display', 'coding.firstObject.system', 'coding.firstObject.code', {
     get() {
-      return this.get('text') || this.get('coding.firstObject.display') || `${this.get('coding.firstObject.system')}-${this.get('coding.firstObject.code')}`;
+      return this.get('text') || this.get('coding.firstObject.display') || `${SYSTEM_LOOKUP[this.get('coding.firstObject.system')] || this.get('coding.firstObject.system')} ${this.get('coding.firstObject.code')}`;
     }
   }).readOnly()
 });
